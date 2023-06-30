b169c1decbc165f43ee08d1f647f6b778025721f
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-06-30 13:19:08 +0200

    task2 done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict'
 //task1
 
 function createSum() {
@@ -14,3 +15,32 @@ function createSum() {
   console.log(sum(3)); // Виведе: 3
   console.log(sum(5)); //виведе 8
   console.log(sum(20)); // Виведе: 28
+
+  //task2
+
+  //1. визначити, чи елемент - число(за допомогою умови, в якій використовуємо typeof)
+  //2. Пройтись циклом по числах, зібрати суму
+  //3. порахувати кількість числових елементів
+
+  let array = ["moon", 2, 5, 10, "sun"]
+
+  function average(array){
+    let sum = 0;
+    let count = 0;
+
+    
+        for(let i = 0; i < array.length; i++){
+            if (typeof array[i] === 'number'){
+            sum += array[i];
+            count++;
+            }
+        }
+        const average = sum / count;
+        return average;
+    
+   
+  }
+
+  
+
+  console.log(average(array));
